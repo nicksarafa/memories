51ce1035b637cf8b4b96b02eb3a630ee2c37c001
nsarafa <nicksarafa@gmail.com>
2017-05-22 23:15:54 -0400

    pass correctPassword as .env var

--- src/stores/PasswordStore/PasswordStore.js
+++ src/stores/PasswordStore/PasswordStore.js
@@ -6,9 +6,18 @@ import { action, observable } from 'mobx'
 class PasswordStore {
   @observable passwordValue = ''
 
+  constructor() {
+    this.correctPassword = process.env.REACT_APP_CORRECT_PASSWORD
+  }
+
   @action _handlePasswordChange(password) {
     this.passwordValue = password
     console.log('password', password)
+
+    if (password === this.correctPassword) {
+      console.log('correct', password)
+      console.log('correctPassword', this.correctPassword)
+    }
   }
 
   /**
